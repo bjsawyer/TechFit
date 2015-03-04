933614dbefebf86adc4f7d80fbc309b4126ece47
Brendan Sawyer <bjsawyer@vt.edu>
2015-03-03 22:37:06 -0500

    fixed up footer

--- footer.php
+++ footer.php
@@ -2,6 +2,8 @@
 	<div class="inner">
 		<a href="#">About</a>
 		<text> | </text>
+		<a href="#">Advertise</a>
+		<text> | </text>
 		<a href="#">Contact</a>
 		<text> | </text>
 		<a href="#">Support</a>
