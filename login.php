11dc604df15cbb7e7b2c648a0af9e6336a06869e
Brendan Sawyer <bjsawyer@vt.edu>
2015-03-04 20:11:47 -0500

    more UI changes to the login page and header

--- login.php
+++ login.php
@@ -18,83 +18,91 @@
 				include('navMenu.php');
 			?>
 				<div class="inner cover">
-					<div>
-						<h2>Existing user?</h2>
-						<h4>Sign in here:</h4>
-						<form class="form-inline">
-							<div class="row row-padding">
-								<div class="form-group">
-									<label class="sr-only" for="userExistingEmail">Email</label>
-									<input type="email" class="form-control" id="userExistingEmail" placeholder="Email">
-								</div>
-								<div class="form-group">
-									<label class="sr-only" for="userExistingPassword">Password:</label>
-									<input type="password" class="form-control" id="userExistingPassword" placeholder="Password">
-								</div>
-							</div>
-							<div class="row row-padding">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox"> Keep me logged in
-									</label>
-								</div>
-							</div>
-							<div class="row">
-								<button type="submit" class="btn btn-default">Log in</button>
-							</div>
-						</form>
-					</div>
-					<div>
-						<h2>New user?</h2>
-						<h4>Create account here:</h4>
-						<form class="form-horizontal">
-							<div class="form-group">
-								<div class="row row-centered row-padding">
-									<label class="sr-only" for="userFirstName">First Name</label>
-									<div class="col-sm-6 col-centered">
-	                                    <input type="text" class="form-control" id="userFirstName" placeholder="First name">
-	                                    </div>
-	                                </div>
-	                                <div class="row row-centered row-padding">
-									<label class="sr-only" for="userLastName">Last Name</label>
-									<div class="col-sm-6 col-centered">
-										<input type="text" class="form-control" id="userLastName" placeholder="Last name">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h2 class="panel-title"><strong>Existing user?</strong></h2>
+						</div>
+						<div class="panel-body">
+							<h4>Sign in here:</h4>
+							<form class="form-inline">
+								<div class="row row-padding">
+									<div class="form-group">
+										<label class="sr-only" for="userExistingEmail">Email</label>
+										<input type="email" class="form-control" id="userExistingEmail" placeholder="Email">
 									</div>
-								</div>
-								<div class="row row-centered row-padding">
-									<label class="sr-only" for="userNewEmail">Email</label>
-									<div class="col-sm-6 col-centered">
-	                                        <input type="email" class="form-control" id="userNewEmail" placeholder="Email">
-	                                    </div>
-								</div>
-								<div class="row row-centered row-padding">
-									<label class="sr-only" for="userNewPassword">Password</label>
-									<div class="col-sm-6 col-centered">
-										<input type="password" class="form-control" id="userNewPassword" placeholder="Password">
-									</div>
-								</div>
-								<div class="row row-centered">
-									<label class="sr-only"  for="type">Account type</label>
-									<div class="col-sm-6 col-centered">
-										<select class="form-control" id="accountType">
-											<option value="trainer">I am a trainer or gym</option>
-											<option value="user">I am looking for a trainer or gym</option>
-										</select>
+									<div class="form-group">
+										<label class="sr-only" for="userExistingPassword">Password:</label>
+										<input type="password" class="form-control" id="userExistingPassword" placeholder="Password">
 									</div>
 								</div>
 								<div class="row row-padding">
 									<div class="checkbox">
 										<label>
-											<input type="checkbox"> I agree to the Terms & Conditions
+											<input type="checkbox"> Keep me logged in
 										</label>
 									</div>
 								</div>
 								<div class="row">
-									<button type="submit" class="btn btn-default">Create</button>
+									<button type="submit" class="btn btn-default">Log in</button>
 								</div>
+							</form>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h2 class="panel-title"><strong>New user?</strong></h2>
+						</div>
+						<div class="panel-body">
+							<h4>Create account here:</h4>
+							<form class="form-horizontal">
+								<div class="form-group">
+									<div class="row row-centered row-padding">
+										<label class="sr-only" for="userFirstName">First Name</label>
+										<div class="col-sm-6 col-centered">
+		                                    <input type="text" class="form-control" id="userFirstName" placeholder="First name">
+		                                    </div>
+		                                </div>
+		                                <div class="row row-centered row-padding">
+										<label class="sr-only" for="userLastName">Last Name</label>
+										<div class="col-sm-6 col-centered">
+											<input type="text" class="form-control" id="userLastName" placeholder="Last name">
+										</div>
+									</div>
+									<div class="row row-centered row-padding">
+										<label class="sr-only" for="userNewEmail">Email</label>
+										<div class="col-sm-6 col-centered">
+		                                        <input type="email" class="form-control" id="userNewEmail" placeholder="Email">
+		                                    </div>
+									</div>
+									<div class="row row-centered row-padding">
+										<label class="sr-only" for="userNewPassword">Password</label>
+										<div class="col-sm-6 col-centered">
+											<input type="password" class="form-control" id="userNewPassword" placeholder="Password">
+										</div>
+									</div>
+									<div class="row row-centered">
+										<label class="sr-only"  for="type">Account type</label>
+										<div class="col-sm-6 col-centered">
+											<select class="form-control" id="accountType">
+												<option value="trainer">I am a trainer or gym</option>
+												<option value="user">I am looking for a trainer or gym</option>
+											</select>
+										</div>
+									</div>
+									<div class="row row-padding">
+										<div class="checkbox">
+											<label>
+												<input type="checkbox"> I agree to the Terms & Conditions
+											</label>
+										</div>
+									</div>
+									<div class="row">
+										<button type="submit" class="btn btn-default">Create</button>
+									</div>
+									</div>
 								</div>
-							</div>
-						</form>
+							</form>
+						</div>
 					</div>
 					<?
 						include('footer.php');
--- navMenu.php
+++ navMenu.php
@@ -4,10 +4,10 @@
 			<img alt="TechFit" src="techfit_icon.png">
 		</a>
         <nav>
-            <ul class="nav masthead-nav">
-                <li class="active"><a href="#">Home</a></li>
-		        <li><a href="#">Profile</a></li>
-		        <li><a href="#">Listings</a></li>
+            <ul class="nav nav-pills" id="nav-pills-format">
+                <li class="active"><a href="#" name="Home"><span class="glyphicon glyphicon-home"></span></a></li>
+		        <li><a href="#"><span name="Listings" class="glyphicon glyphicon-th-list"></span></a></li>
+		        <li><a href="#"><span name="Search" class="glyphicon glyphicon-search"></span></a></li>
             </ul>
         </nav>
   </div>
