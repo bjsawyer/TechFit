95b0e3149680bbfdcd6aa7df0b91d853a666177f
Brendan Sawyer <bjsawyer@vt.edu>
2015-04-22 14:23:15 -0400

    added labels for each form input

--- helpers/createNewTrainer.php
+++ helpers/createNewTrainer.php
@@ -1,50 +1,64 @@
 <? function renderTrainerForm() { ?>
 		<form id="trainerForm" class="form-horizontal"  method="POST" action="index.php">
-			<h4>Sign up as a <b>TRAINER</b></h4>
 			<fieldset>
 				<div class="form-group">
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newFirstName">First name</label>
                             <input type="text" class="form-control" id="newFirstName" name="newFirstName" placeholder="First name">
                         </div>
                     </div>
                     <div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newLastName">Last name</label>
 							<input type="text" class="form-control" id="newLastName" name="newLastName" placeholder="Last name">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newEmail">Email</label>
 		                    <input type="email" class="form-control" id="newEmail" name="newEmail" placeholder="Email">
 		                </div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newPassword">Password</label>
 							<input type="password" class="form-control" id="newPassword" name="newPassword" placeholder="Password">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newConfirmPassword">Confirm password</label>
+							<input type="password" class="form-control" id="newConfirmPassword" name="newConfirmPassword" placeholder="Confirm password">
+						</div>
+					</div>
+					<div class="row row-centered row-padding">
+						<div class="col-sm-6 col-centered">
+							<label for="newAddress">Address</label>
 							<input type="text" class="form-control" id="newAddress" name="newAddress" placeholder="Address">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newCity">City</label>
 							<input type="text" class="form-control" id="newCity" name="newCity" placeholder="City">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newState">State</label>
 							<select class="form-control" id="newState" name="newState"></select>
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newZip">Zip</label>
 							<input type="text" class="form-control" id="newZip" name="newZip" placeholder="Zip">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newPhone">Phone</label>
 							<input type="text" class="form-control" id="newPhoneNumber" name="newPhoneNumber" placeholder="Phone">
 						</div>
 					</div>
--- helpers/createNewUser.php
+++ helpers/createNewUser.php
@@ -1,55 +1,70 @@
 <? function renderUserForm() { ?>
 		<form id="userForm" class="form-horizontal"  method="POST" action="createAccountSuccess.php">
-			<h4>Sign up as a <b>USER</b></h4>
 			<fieldset>
 				<div class="form-group">
-					<div class="row row-centered row-padding">
+										<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newFirstName">First name</label>
                             <input type="text" class="form-control" id="newFirstName" name="newFirstName" placeholder="First name">
                         </div>
                     </div>
                     <div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newLastName">Last name</label>
 							<input type="text" class="form-control" id="newLastName" name="newLastName" placeholder="Last name">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newEmail">Email</label>
 		                    <input type="email" class="form-control" id="newEmail" name="newEmail" placeholder="Email">
 		                </div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newPassword">Password</label>
 							<input type="password" class="form-control" id="newPassword" name="newPassword" placeholder="Password">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newConfirmPassword">Confirm password</label>
+							<input type="password" class="form-control" id="newConfirmPassword" name="newConfirmPassword" placeholder="Confirm password">
+						</div>
+					</div>
+					<div class="row row-centered row-padding">
+						<div class="col-sm-6 col-centered">
+							<label for="newAddress">Address</label>
 							<input type="text" class="form-control" id="newAddress" name="newAddress" placeholder="Address">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newCity">City</label>
 							<input type="text" class="form-control" id="newCity" name="newCity" placeholder="City">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newState">State</label>
 							<select class="form-control" id="newState" name="newState"></select>
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newZip">Zip</label>
 							<input type="text" class="form-control" id="newZip" name="newZip" placeholder="Zip">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
-							<input type="text" class="form-control" id="newPhone" name="newPhone" placeholder="Phone">
+							<label for="newPhone">Phone</label>
+							<input type="text" class="form-control" id="newPhoneNumber" name="newPhoneNumber" placeholder="Phone">
 						</div>
 					</div>
 					<div class="row row-centered row-padding">
 						<div class="col-sm-6 col-centered">
+							<label for="newSearchingFor">Searching for</label>
 							<select class="form-control" id="newSearchingFor" name="newSearchingFor">
 								<option value="" selected disabled>Searching for</option>
 								<option value="Trainer">Trainer</option>
