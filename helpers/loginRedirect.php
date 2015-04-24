60ee9cdf21ad0a83953db9e965bf37246dd2320f
Brendan Sawyer <bjsawyer@vt.edu>
2015-04-24 00:06:15 -0400

    removed unused line

--- helpers/loginRedirect.php
+++ helpers/loginRedirect.php
@@ -8,8 +8,6 @@
 	include('autoLogin.php');
 	echo  loginToUserOrProviderAccount();
 	
-	//print_r($_SESSION["account_record"]);
-	
 	if ($_SESSION["new_account_type"] == "User") {
 		header('location: ../index.php');
 	}else if ($_SESSION["new_account_type"] == "Trainer" || $_SESSION["new_account_type"] == "Gym") {
