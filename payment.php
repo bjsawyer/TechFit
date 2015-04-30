ad25c42659fb2044b8bbe04e69f06845c81d8588
Brendan Sawyer <bjsawyer@vt.edu>
2015-04-30 09:00:06 -0400

    added paypal to payment.php

--- payment.php
+++ payment.php
@@ -35,13 +35,24 @@
 						include('templates/sidebarRight.php');
 					?>
 					<div class="inner cover text-left">
-						<div class="panel panel-default">
-							<div class="panel-heading"><h3><b>Premium Plan Payment</b></h3></div>
-							<div class="panel-body text-center" style="padding:50px 15px; font-size: 16px">
-								<i>*NOTE: Payment system to be implemented in a future release.</i>
-								<br>
-								<br>
-								Click <a href="index.php" style="color:blue">here</a> to go to the home page.</a>
+						<div class="jumbotron" id="login-jumbotron" style="padding-bottom:1px">
+							<h1><b>Pay now!</b></h1>
+							<p class="text-center" style="font-size:14px;margin:30 0;">To pay for your new premium account, select your membership level below, click "Subscribe" and follow the instructions on Paypal.</p>
+							<div class="list-group" style="padding-bottom:50px">
+								<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+									<input type="hidden" name="cmd" value="_s-xclick">
+									<input type="hidden" name="hosted_button_id" value="4SYQCBWQWL2ZQ">
+									<table style="margin-left:auto;margin-right:auto;margin-bottom:5px;">
+										<tr><td><input type="hidden" name="on0" value=""></td></tr><tr><td><select name="os0">
+											<option value="Platinum">Platinum : $25.00 USD - monthly</option>
+											<option value="Gold">Gold : $15.00 USD - monthly</option>
+											<option value="Silver">Silver : $5.00 USD - monthly</option>
+										</select> </td></tr>
+									</table>
+									<input type="hidden" name="currency_code" value="USD">
+									<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+									<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+								</form>
 							</div>
 						</div>
 					</div>
