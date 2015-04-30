ec54ef056a36376efa10494aefc17b8ba037da91
Brendan Sawyer <bjsawyer@vt.edu>
2015-04-29 21:49:03 -0400

    added data validation for each createAccount & update form

--- helpers/newGymInformation.php
+++ helpers/newGymInformation.php
@@ -1,81 +1,81 @@
-<form id="gymForm" class="form-horizontal hidden"  method="POST" action="">
+<form id="gymForm" class="form-horizontal hidden"  method="POST" action="" onsubmit="return checkInputs();">
 	<fieldset>
 		<div class="form-group">
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newGymName">Gym name</label>
-                         <input type="text" class="form-control" id="newGymName" name="newGymName" placeholder="Gym name">
+					<label for="newGymName" class="required-label">Gym name</label>
+                         <input type="text" class="form-control required" id="newGymName" name="newGymName" placeholder="Gym name">
                     </div>
                </div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newEmail">Email</label>
-                         <input type="email" class="form-control" id="newEmail" name="newEmail" placeholder="Email">
+					<label for="newEmail" class="required-label">Email</label>
+                         <input type="email" class="form-control required" id="newEmail" name="newEmail" placeholder="Email">
                     </div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newPassword">Password</label>
-					<input type="password" class="form-control" id="newPassword" name="newPassword" placeholder="Password">
+					<label for="newPassword" class="required-label">Password</label>
+					<input type="password" class="form-control required" id="newPassword" name="newPassword" placeholder="Password">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newConfirmPassword">Confirm password</label>
-					<input type="password" class="form-control" id="newConfirmPassword" name="newConfirmPassword" placeholder="Confirm password">
+					<label for="newConfirmPassword" class="required-label">Confirm password</label>
+					<input type="password" class="form-control required" id="newConfirmPassword" name="newConfirmPassword" placeholder="Confirm password">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newAddress">Address</label>
-					<input type="text" class="form-control" id="newAddress" name="newAddress" placeholder="Address">
+					<label for="newAddress" class="required-label">Address</label>
+					<input type="text" class="form-control required" id="newAddress" name="newAddress" placeholder="Address">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newCity">City</label>
-					<input type="text" class="form-control" id="newCity" name="newCity" placeholder="City">
+					<label for="newCity" class="required-label">City</label>
+					<input type="text" class="form-control required" id="newCity" name="newCity" placeholder="City">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newState">State</label>
-					<select class="form-control newState"  name="newState"></select>
+					<label for="newState" class="required-label">State</label>
+					<select class="form-control required newState"  name="newState"></select>
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newZip">Zip</label>
-					<input type="text" class="form-control" id="newZip" name="newZip" placeholder="Zip">
+					<label for="newZip" class="required-label">Zip</label>
+					<input type="text" class="form-control required" id="newZip" name="newZip" placeholder="Zip">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newPhone">Phone</label>
-					<input type="text" class="form-control" id="newPhoneNumber" name="newPhone" placeholder="Phone">
+					<label for="newPhone" class="required-label">Phone</label>
+					<input type="text" class="form-control required" id="newPhoneNumber" name="newPhone" placeholder="Phone">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newContactFirstName">Contact first name</label>
-                         <input type="text" class="form-control" id="newContactFirstName" name="newContactFirstName" placeholder="Contact first name">
+					<label for="newContactFirstName" class="required-label">Contact first name</label>
+                         <input type="text" class="form-control required" id="newContactFirstName" name="newContactFirstName" placeholder="Contact first name">
                     </div>
                </div>
                
                <div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newContactLastName">Contact last name</label>
-					<input type="text" class="form-control" id="newContactLastName" name="newContactLastName" placeholder="Contact last name">
+					<label for="newContactLastName" class="required-label">Contact last name</label>
+					<input type="text" class="form-control required" id="newContactLastName" name="newContactLastName" placeholder="Contact last name">
 				</div>
 			</div>
 			
@@ -160,7 +160,32 @@
 		</div>
 		
 		<div class="row row-button">
-			<button type="submit" name="newGymSubmit" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Create</button>
+			<button type="submit" id="newGymSubmit" name="newGymSubmit" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Create</button>
 		</div>
 	</fieldset>
 </form>
+<script>	
+	// data validation
+	function checkInputs() {
+		var submitOk = true;
+		
+		$('.required').each(function() {
+			if ($(this).val() == "") {
+				alert("Fields marked with an asterisk (*) are required. Please enter information and try again.");
+				submitOk = false;
+				return false;
+			}			
+		});
+		
+		if (submitOk == true) {
+			var password = $('#newPassword').val();
+			var passwordConfirm = $('#newConfirmPassword').val();
+			if (password != passwordConfirm) {
+				alert("Passwords do not match. Please re-enter password password fields and try again.");
+				submitOk = false;
+			}
+		}
+		
+		return submitOk;
+	};
+</script>
--- helpers/newTrainerInformation.php
+++ helpers/newTrainerInformation.php
@@ -1,18 +1,18 @@
-<form id="trainerForm" class="form-horizontal hidden"  method="POST" action="">
+<form id="trainerForm" class="form-horizontal hidden"  method="POST" action="" onsubmit="return checkInputs();">
 	<fieldset>
 		<div class="form-group">
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newFirstName">First name</label>
-                         <input type="text" class="form-control" id="newFirstName" name="newFirstName" placeholder="First name">
+					<label for="newFirstName" class="required-label">First name</label>
+                         <input type="text" class="form-control required" id="newFirstName" name="newFirstName" placeholder="First name">
                     </div>
                </div>
                
                <div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newLastName">Last name</label>
-					<input type="text" class="form-control" id="newLastName" name="newLastName" placeholder="Last name">
+					<label for="newLastName" class="required-label">Last name</label>
+					<input type="text" class="form-control required" id="newLastName" name="newLastName" placeholder="Last name">
 				</div>
 			</div>
 			
@@ -29,57 +29,57 @@
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newEmail">Email</label>
-                         <input type="email" class="form-control" id="newEmail" name="newEmail" placeholder="Email">
+					<label for="newEmail" class="required-label">Email</label>
+                         <input type="email" class="form-control required" id="newEmail" name="newEmail" placeholder="Email">
                     </div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newPassword">Password</label>
-					<input type="password" class="form-control" id="newPassword" name="newPassword" placeholder="Password">
+					<label for="newPassword" class="required-label">Password</label>
+					<input type="password" class="form-control required" id="newPassword" name="newPassword" placeholder="Password">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newConfirmPassword">Confirm password</label>
-					<input type="password" class="form-control" id="newConfirmPassword" name="newConfirmPassword" placeholder="Confirm password">
+					<label for="newConfirmPassword" class="required-label">Confirm password</label>
+					<input type="password" class="form-control required" id="newConfirmPassword" name="newConfirmPassword" placeholder="Confirm password">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newAddress">Address</label>
-					<input type="text" class="form-control" id="newAddress" name="newAddress" placeholder="Address">
+					<label for="newAddress" class="required-label">Address</label>
+					<input type="text" class="form-control required" id="newAddress" name="newAddress" placeholder="Address">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newCity">City</label>
-					<input type="text" class="form-control" id="newCity" name="newCity" placeholder="City">
+					<label for="newCity" class="required-label">City</label>
+					<input type="text" class="form-control required" id="newCity" name="newCity" placeholder="City">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newState">State</label>
-					<select class="form-control newState"  name="newState"></select>
+					<label for="newState" class="required-label">State</label>
+					<select class="form-control required newState"  name="newState"></select>
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newZip">Zip</label>
-					<input type="text" class="form-control" id="newZip" name="newZip" placeholder="Zip">
+					<label for="newZip" class="required-label">Zip</label>
+					<input type="text" class="form-control required" id="newZip" name="newZip" placeholder="Zip">
 				</div>
 			</div>
 			
 			<div class="row row-centered row-padding">
 				<div class="col-sm-6 col-centered">
-					<label for="newPhone">Phone</label>
-					<input type="text" class="form-control" id="newPhoneNumber" name="newPhone" placeholder="Phone">
+					<label for="newPhone" class="required-label">Phone</label>
+					<input type="text" class="form-control required" id="newPhoneNumber" name="newPhone" placeholder="Phone">
 				</div>
 			</div>
 			
@@ -167,8 +167,33 @@
 		</div>
 		
 		<div class="row row-button">
-			<button type="submit" name="newTrainerSubmit" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Create</button>
+			<button type="submit"id="newTrainerSubmit" name="newTrainerSubmit" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Create</button>
 		</div>
 		
 	</fieldset>
 </form>
+<script>	
+	// data validation
+	function checkInputs() {
+		var submitOk = true;
+		
+		$('.required').each(function() {
+			if ($(this).val() == "") {
+				alert("Fields marked with an asterisk (*) are required. Please enter information and try again.");
+				submitOk = false;
+				return false;
+			}			
+		});
+		
+		if (submitOk == true) {
+			var password = $('#newPassword').val();
+			var passwordConfirm = $('#newConfirmPassword').val();
+			if (password != passwordConfirm) {
+				alert("Passwords do not match. Please re-enter password password fields and try again.");
+				submitOk = false;
+			}
+		}
+		
+		return submitOk;
+	};
+</script>
